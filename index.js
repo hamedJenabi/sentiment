5645984cb8130807d20f764adadd133e56250c11
Hamed <hamed.jenabi@gmail.com>
2020-05-07 17:30:54 +0200

    Sentiment

---
+++ index.js
@@ -0,0 +1,47 @@
+const got = require('got');
+const request = require('request');
+const https = require('https');
+var fs = require('fs');
+
+
+
+
+
+
+
+
+
+
+
+/*
+const data = JSON.stringify({
+  todo: 'I am very sad',
+});
+
+const options = {
+  hostname: 'api.theysay.io',
+  port: 443,
+  path: '/',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': 38,
+  },
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('I am sad', (d) => {
+    process.stdout.write(d);
+    console.log('Response:', body);
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.write(data);
+req.end();
+*/
